616ef76e78895de52d1a6597189aade69c70fd94
Ria Dinesh <riadinesh@Rias-MacBook-Pro.local>
2026-05-29 00:06:15 -0700

    Switching from sqlite to postgresql

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./projectDatabase.db"
+# DATABASE_URL = "sqlite:///./projectDatabase.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
